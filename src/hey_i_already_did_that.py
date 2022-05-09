0a12cb9f35d0d132b2971483b0840b49e1b0b79b
Partha Majumdar <partha.majumdar@keysight.com>
2022-05-09 14:37:12 -0700

    Added a decription of the minion taks assignment problem.

--- src/hey_i_already_did_that.py
+++ src/hey_i_already_did_that.py
@@ -1,3 +1,27 @@
+'''
+Problem Description:
+===================
+
+Commander lambda assigns tasks his minions. Each minion has a id number.
+Those id numbers are of length k and digits are in base b.
+
+This is how commander lambda assigns the tasks. He picks up a task and
+randomly chooses a minion id n. After miniom n is done with the task,
+the commander chooses the next minion for doing this task in the following
+way.
+
+He forms a number x by arranging the digits on n in descending order and
+forms another number y by arranging the digits on n in ascending order.
+The he calculates the next minion id as z = x - y. If the number of digits
+in z is less than k then number is padded with 0 in the most significant
+positions.
+
+We need to avoid the situation where the task may get re-assigned to a
+minion who has already done it. We need to find out after how many
+iterations the task will keep coming back to a minion for any random
+starting minion id.
+'''
+
 def ToInt(digits, b):
     result = 0
     mul    = 1
